61b78213b57c8d7bc4290e6189006bb961a3fdd4
Dasister <maxsyrman@yandex.ru>
2019-09-08 18:39:05 +0300

    Initial commit

---
+++ src/main/java/com/github/dasister/sourcebans/SourcebansApplication.java
@@ -0,0 +1,13 @@
+package com.github.dasister.sourcebans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcebansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcebansApplication.class, args);
+	}
+
+}
